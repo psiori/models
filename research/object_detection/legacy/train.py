effba3243c35a0dc39f09e4c42bc001a92f2743d
Sara <sarah.jamal86@gmail.com>
2020-05-05 15:26:10 +0200

    mini change

--- research/object_detection/legacy/train.py
+++ research/object_detection/legacy/train.py
@@ -45,141 +45,194 @@ import functools
 import json
 import os
 import tensorflow as tf
-from tensorflow.contrib import framework as contrib_framework
 
-from object_detection.builders import dataset_builder
-from object_detection.builders import graph_rewriter_builder
-from object_detection.builders import model_builder
+from google.protobuf import text_format
+
 from object_detection.legacy import trainer
-from object_detection.utils import config_util
+from object_detection.builders import input_reader_builder
+from object_detection.builders import model_builder
+from object_detection.protos import input_reader_pb2
+from object_detection.protos import model_pb2
+from object_detection.protos import pipeline_pb2
+from object_detection.protos import train_pb2
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
 flags = tf.app.flags
-flags.DEFINE_string('master', '', 'Name of the TensorFlow master to use.')
-flags.DEFINE_integer('task', 0, 'task id')
-flags.DEFINE_integer('num_clones', 1, 'Number of clones to deploy per worker.')
-flags.DEFINE_boolean('clone_on_cpu', False,
-                     'Force clones to be deployed on CPU.  Note that even if '
-                     'set to False (allowing ops to run on gpu), some ops may '
-                     'still be run on the CPU if they have no GPU kernel.')
-flags.DEFINE_integer('worker_replicas', 1, 'Number of worker+trainer '
-                     'replicas.')
-flags.DEFINE_integer('ps_tasks', 0,
-                     'Number of parameter server tasks. If None, does not use '
-                     'a parameter server.')
-flags.DEFINE_string('train_dir', '',
-                    'Directory to save the checkpoints and training summaries.')
+flags.DEFINE_string("master", "", "BNS name of the TensorFlow master to use.")
+flags.DEFINE_integer("task", 0, "task id")
+flags.DEFINE_integer("num_clones", 1, "Number of clones to deploy per worker.")
+flags.DEFINE_boolean(
+    "clone_on_cpu",
+    False,
+    "Force clones to be deployed on CPU.  Note that even if "
+    "set to False (allowing ops to run on gpu), some ops may "
+    "still be run on the CPU if they have no GPU kernel.",
+)
+flags.DEFINE_integer(
+    "worker_replicas", 1, "Number of worker+trainer " "replicas."
+)
+flags.DEFINE_integer(
+    "ps_tasks",
+    0,
+    "Number of parameter server tasks. If None, does not use "
+    "a parameter server.",
+)
+flags.DEFINE_string(
+    "train_dir",
+    "/Users/sarajamal/psipy_computervision/data/detect/logs/train_dir",
+    "Directory to save the checkpoints and training summaries.",
+)
 
-flags.DEFINE_string('pipeline_config_path', '',
-                    'Path to a pipeline_pb2.TrainEvalPipelineConfig config '
-                    'file. If provided, other configs are ignored')
+flags.DEFINE_string(
+    "pipeline_config_path",
+    "/Users/sarajamal/psipy_computervision/data/detect/ssd_mobilenet_v2_coco.config",
+    "Path to a pipeline_pb2.TrainEvalPipelineConfig config "
+    "file. If provided, other configs are ignored",
+)
 
-flags.DEFINE_string('train_config_path', '',
-                    'Path to a train_pb2.TrainConfig config file.')
-flags.DEFINE_string('input_config_path', '',
-                    'Path to an input_reader_pb2.InputReader config file.')
-flags.DEFINE_string('model_config_path', '',
-                    'Path to a model_pb2.DetectionModel config file.')
+flags.DEFINE_string(
+    "train_config_path", "",
+    "Path to a train_pb2.TrainConfig config file."
+)
+flags.DEFINE_string(
+    "input_config_path",
+    "",
+    "Path to an input_reader_pb2.InputReader config file.",
+)
+flags.DEFINE_string(
+    "model_config_path", "", "Path to a model_pb2.DetectionModel config file."
+)
 
 FLAGS = flags.FLAGS
 
 
-@contrib_framework.deprecated(None, 'Use object_detection/model_main.py.')
+def get_configs_from_pipeline_file():
+    """Reads training configuration from a pipeline_pb2.TrainEvalPipelineConfig.
+
+  Reads training config from file specified by pipeline_config_path flag.
+
+  Returns:
+    model_config: model_pb2.DetectionModel
+    train_config: train_pb2.TrainConfig
+    input_config: input_reader_pb2.InputReader
+  """
+    pipeline_config = pipeline_pb2.TrainEvalPipelineConfig()
+    with tf.gfile.GFile(FLAGS.pipeline_config_path, "r") as f:
+        text_format.Merge(f.read(), pipeline_config)
+
+    model_config = pipeline_config.model
+    train_config = pipeline_config.train_config
+    input_config = pipeline_config.train_input_reader
+
+    return model_config, train_config, input_config
+
+
+def get_configs_from_multiple_files():
+    """Reads training configuration from multiple config files.
+
+  Reads the training config from the following files:
+    model_config: Read from --model_config_path
+    train_config: Read from --train_config_path
+    input_config: Read from --input_config_path
+
+  Returns:
+    model_config: model_pb2.DetectionModel
+    train_config: train_pb2.TrainConfig
+    input_config: input_reader_pb2.InputReader
+  """
+    train_config = train_pb2.TrainConfig()
+    with tf.gfile.GFile(FLAGS.train_config_path, "r") as f:
+        text_format.Merge(f.read(), train_config)
+
+    model_config = model_pb2.DetectionModel()
+    with tf.gfile.GFile(FLAGS.model_config_path, "r") as f:
+        text_format.Merge(f.read(), model_config)
+
+    input_config = input_reader_pb2.InputReader()
+    with tf.gfile.GFile(FLAGS.input_config_path, "r") as f:
+        text_format.Merge(f.read(), input_config)
+
+    return model_config, train_config, input_config
+
+
 def main(_):
-  assert FLAGS.train_dir, '`train_dir` is missing.'
-  if FLAGS.task == 0: tf.gfile.MakeDirs(FLAGS.train_dir)
-  if FLAGS.pipeline_config_path:
-    configs = config_util.get_configs_from_pipeline_file(
-        FLAGS.pipeline_config_path)
-    if FLAGS.task == 0:
-      tf.gfile.Copy(FLAGS.pipeline_config_path,
-                    os.path.join(FLAGS.train_dir, 'pipeline.config'),
-                    overwrite=True)
-  else:
-    configs = config_util.get_configs_from_multiple_files(
-        model_config_path=FLAGS.model_config_path,
-        train_config_path=FLAGS.train_config_path,
-        train_input_config_path=FLAGS.input_config_path)
-    if FLAGS.task == 0:
-      for name, config in [('model.config', FLAGS.model_config_path),
-                           ('train.config', FLAGS.train_config_path),
-                           ('input.config', FLAGS.input_config_path)]:
-        tf.gfile.Copy(config, os.path.join(FLAGS.train_dir, name),
-                      overwrite=True)
+    assert FLAGS.train_dir, "`train_dir` is missing."
+    if FLAGS.pipeline_config_path:
+        model_config, train_config, input_config = (
+            get_configs_from_pipeline_file()
+        )
+    else:
+        model_config, train_config, input_config = (
+            get_configs_from_multiple_files()
+        )
 
-  model_config = configs['model']
-  train_config = configs['train_config']
-  input_config = configs['train_input_config']
+    model_fn = functools.partial(
+        model_builder.build, model_config=model_config, is_training=True
+    )
 
-  model_fn = functools.partial(
-      model_builder.build,
-      model_config=model_config,
-      is_training=True)
+    create_input_dict_fn = functools.partial(
+        input_reader_builder.build, input_config
+    )
 
-  def get_next(config):
-    return dataset_builder.make_initializable_iterator(
-        dataset_builder.build(config)).get_next()
+    env = json.loads(os.environ.get("TF_CONFIG", "{}"))
+    cluster_data = env.get("cluster", None)
+    cluster = tf.train.ClusterSpec(cluster_data) if cluster_data else None
+    task_data = env.get("task", None) or {"type": "master", "index": 0}
+    task_info = type("TaskSpec", (object,), task_data)
 
-  create_input_dict_fn = functools.partial(get_next, input_config)
+    # Parameters for a single worker.
+    ps_tasks = 0
+    worker_replicas = 1
+    worker_job_name = "lonely_worker"
+    task = 0
+    is_chief = True
+    master = ""
 
-  env = json.loads(os.environ.get('TF_CONFIG', '{}'))
-  cluster_data = env.get('cluster', None)
-  cluster = tf.train.ClusterSpec(cluster_data) if cluster_data else None
-  task_data = env.get('task', None) or {'type': 'master', 'index': 0}
-  task_info = type('TaskSpec', (object,), task_data)
+    if cluster_data and "worker" in cluster_data:
+        # Number of total worker replicas include "worker"s and the "master".
+        worker_replicas = len(cluster_data["worker"]) + 1
+    if cluster_data and "ps" in cluster_data:
+        ps_tasks = len(cluster_data["ps"])
 
-  # Parameters for a single worker.
-  ps_tasks = 0
-  worker_replicas = 1
-  worker_job_name = 'lonely_worker'
-  task = 0
-  is_chief = True
-  master = ''
+    if worker_replicas > 1 and ps_tasks < 1:
+        raise ValueError(
+            "At least 1 ps task is needed for distributed training."
+        )
 
-  if cluster_data and 'worker' in cluster_data:
-    # Number of total worker replicas include "worker"s and the "master".
-    worker_replicas = len(cluster_data['worker']) + 1
-  if cluster_data and 'ps' in cluster_data:
-    ps_tasks = len(cluster_data['ps'])
+    if worker_replicas >= 1 and ps_tasks > 0:
+        # Set up distributed training.
+        server = tf.train.Server(
+            tf.train.ClusterSpec(cluster),
+            protocol="grpc",
+            job_name=task_info.type,
+            task_index=task_info.index,
+        )
+        if task_info.type == "ps":
+            server.join()
+            return
 
-  if worker_replicas > 1 and ps_tasks < 1:
-    raise ValueError('At least 1 ps task is needed for distributed training.')
+        worker_job_name = "%s/task:%d" % (task_info.type, task_info.index)
+        task = task_info.index
+        is_chief = task_info.type == "master"
+        master = server.target
 
-  if worker_replicas >= 1 and ps_tasks > 0:
-    # Set up distributed training.
-    server = tf.train.Server(tf.train.ClusterSpec(cluster), protocol='grpc',
-                             job_name=task_info.type,
-                             task_index=task_info.index)
-    if task_info.type == 'ps':
-      server.join()
-      return
-
-    worker_job_name = '%s/task:%d' % (task_info.type, task_info.index)
-    task = task_info.index
-    is_chief = (task_info.type == 'master')
-    master = server.target
-
-  graph_rewriter_fn = None
-  if 'graph_rewriter_config' in configs:
-    graph_rewriter_fn = graph_rewriter_builder.build(
-        configs['graph_rewriter_config'], is_training=True)
-
-  trainer.train(
-      create_input_dict_fn,
-      model_fn,
-      train_config,
-      master,
-      task,
-      FLAGS.num_clones,
-      worker_replicas,
-      FLAGS.clone_on_cpu,
-      ps_tasks,
-      worker_job_name,
-      is_chief,
-      FLAGS.train_dir,
-      graph_hook_fn=graph_rewriter_fn)
+    trainer.train(
+        create_input_dict_fn,
+        model_fn,
+        train_config,
+        master,
+        task,
+        FLAGS.num_clones,
+        worker_replicas,
+        FLAGS.clone_on_cpu,
+        ps_tasks,
+        worker_job_name,
+        is_chief,
+        FLAGS.train_dir,
+    )
 
 
-if __name__ == '__main__':
-  tf.app.run()
+if __name__ == "__main__":
+    tf.app.run()
+
